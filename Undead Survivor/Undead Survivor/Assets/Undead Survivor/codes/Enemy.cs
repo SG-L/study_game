910b768900b5aea198fee2f8cf752e665850344c
sglee <sglee8293@naver.com>
2025-04-04 23:21:20 +0900

    오브젝트 풀링, 몬스터 레벨 적용

--- Undead Survivor/Undead Survivor/Assets/Undead Survivor/codes/Enemy.cs
+++ Undead Survivor/Undead Survivor/Assets/Undead Survivor/codes/Enemy.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed;
+    public float health;
+    public float maxHealth;
+    public RuntimeAnimatorController[] animCon;
     public Rigidbody2D target;
 
-    bool isLive = true;
+    bool isLive;
 
     Rigidbody2D rigid;
+    Animator anim;
     SpriteRenderer spriter;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spriter = GetComponent<SpriteRenderer>();
     }
 
@@ -36,5 +41,20 @@ public class Enemy : MonoBehaviour
         }
 
         spriter.flipX = target.position.x < rigid.position.x;
+    }
+
+    void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+        isLive = true;
+        health = maxHealth;
+    }
+
+    public void Init(SpawnData data)
+    {
+        anim.runtimeAnimatorController = animCon[data.spriteType];
+        speed = data.speed;
+        maxHealth = data.health;
+        health = data.health;
     }
 }
